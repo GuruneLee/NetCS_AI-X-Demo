b70ddf228bd82a7f9d47559c49ca8faf48b1b68c
geumseongYoon <gsyoon@smartx.kr>
2020-07-06 05:55:35 +0000

    Add Dockerfiles

---
+++ Docker/01End/producer_python/start.sh
@@ -0,0 +1,1 @@
+sudo docker run -d --net=host --privileged --device=/dev/video0:/dev/video0 -it --name end_producer geumseongyoon/aix-end-producer:1.0
---
+++ Docker/01End/zookeeper/start.sh
@@ -0,0 +1,1 @@
+sudo docker run -d --net=host --name end-zookeeper geumseongyoon/aix-end-zookeeper:1.0
