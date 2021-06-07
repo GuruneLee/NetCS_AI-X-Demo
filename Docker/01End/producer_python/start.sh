4e0cb4a6d8cc86eaee506e8ed9f763207810e69d
gurunelee <dlckdgk4858@gmail.com>
2021-06-07 19:16:31 +0900

    modify Dockerfiel/start.sh for End-producer

--- Docker/01End/producer_python/start.sh
+++ Docker/01End/producer_python/start.sh
@@ -1,1 +1,1 @@
-sudo docker run -d --net=host --privileged --device=/dev/video0:/dev/video0 -it --name end_producer geumseongyoon/aix-end-producer:1.0
+sudo docker run -d --net=host --privileged --device=/dev/video0:/dev/video0 -it --name end_producer dlckdgk4858/aix-end-producer:dev
