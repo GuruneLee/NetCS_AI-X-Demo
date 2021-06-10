4b4f7c2a3acb5f99d573fe5ab230ab1aa75ff1aa
gurunelee <dlckdgk4858@gmail.com>
2021-06-10 04:15:51 +0000

    modify sage consumer

--- Docker/03Sage/consumer.py
+++ Docker/03Sage/consumer.py
@@ -11,22 +11,24 @@ import time
 consumer2 = KafkaConsumer('my-topic', bootstrap_servers='172.30.84.61:9092', api_version=(0, 10, 1))
 path3 = "/home/netcs/frames/source3/"
 
+app = Flask(__name__)
+
 def kafkastream():
-    yield (b'--frame\r\n'
-            b'Content-Type: image/jpeg\r\n\r\n' + message.value + b'\r\n\r\n')
     print("bcdf")
     count = 0
     video_num = 0
     now = datetime.datetime.now()
     for message in consumer2:
+        yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + message.value + b'\r\n\r\n')
         #print("yogi")
         array = np.frombuffer( message.value, dtype = np.dtype('uint8'))
         image = cv2.imdecode(array,1)
-        cv2.namedWindow("Image", cv2.WND_PROP_FULLSCREEN)
-        cv2.setWindowProperty("Image",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+        #cv2.namedWindow("Image", cv2.WND_PROP_FULLSCREEN)
+        #cv2.setWindowProperty("Image",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
         #cv2.imshow('Image', image)
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            break
+        #if cv2.waitKey(25) & 0xFF == ord('q'):
+        #    break
                 #count += 1;
                 #count_str = str(count)
                 #cv2.imwrite("/home/tein/kafka-python-video-streaming/src/result_img/test_" + count_str  +".jpg", image)
